07308e8c78f8ba3f0d3a56713c14d005ca7b143e
FrancieleCSantos <francielecoceicaodossantos93@gmail.com>
2023-07-04 21:39:08 -0300

    Projeto concluido

--- Fran-Santos/Meu-Projeto-React/src/pages/Home.jsx
+++ Fran-Santos/Meu-Projeto-React/src/pages/Home.jsx
@@ -1,9 +1,7 @@
 import Header from "../components/Header"
 import Title from "../components/Title"
 import styles from '../styles/pages/home.module.css'
-import { FcAddressBook } from "react-icons/fc";
-import { FcLike } from "react-icons/fc";
-import {FcReddit} from "react-icons/fc"
+import Text from '../components/Text'
 import home from '../assets/home.svg'
 
 
@@ -15,30 +13,30 @@ const Home = () => {
         subtitle="Desenvolvedora Fron-End"
         image={home}
       />
-    
+
       <main className={styles.containerMainHome}>
         <div className={styles.cardVida}>
-        <FcLike  className={styles.imgVida} color="#AE2983" weight="fill" size={90} />
+      
           <Title content="Vida" />
-          <p>Eu conto um pouquinho sobre mim, minha formação,
+          <Text text="Eu conto um pouquinho sobre mim, minha formação,
             a história da minha migração de carreira,
-            gostos e curiosidades.
-          </p>
+            gostos e curiosidades.">
+          </Text>
         </div>
         <div className={styles.cardCodigo}>
-         <FcReddit className={styles.imgCodigo} color="#AE2983" weight="fill" size={90}/>
+       
           <Title content="Código" />
-          <p>Mostro meu portfólio. Tudo o que aprendi até o momento dando
+          <Text text="Mostro meu portfólio. Tudo o que aprendi até o momento dando
             destaque para alguns dos projetos que criei enquanto estudo
-            programação.
-          </p>
+            programação.">
+          </Text>
         </div>
         <div className={styles.cardContatos}>
-        <FcAddressBook className={styles.imgContatos}color="#AE2983" weight="fill" size={90}/>
+      
           <Title content="Contatos" />
-          <p>Entre em contato comigo. Seja para entrevistas de emprego
-            como deva ou para trocarmos ideias sobre os estudos.
-          </p>
+          <Text text="Entre em contato comigo. Seja para entrevistas de emprego
+            como deva ou para trocarmos ideias sobre os estudos.">
+          </Text>
         </div>
       </main>
     </>
