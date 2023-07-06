250d7709c8d569663b5edfaf96cebb2a8d0ad10b
Náira Nogueira <naira.nogueira24@gmail.com>
2023-07-06 16:59:08 -0300

    Alterações no css e no componente portfolio

--- naira-nogueira/projeto-meu-site-pessoal/src/pages/Portfolio.jsx
+++ naira-nogueira/projeto-meu-site-pessoal/src/pages/Portfolio.jsx
@@ -7,6 +7,10 @@ import { useEffect, useState } from "react"
 const Portfolio = () => {
     const [repos, setRepos] = useState([])
 
+    let ids = ['625515839', '628268501', '632151063']
+
+    let listaRepoQuero = repos.filter(item => ids.find(id => id== item.id))
+
     useEffect(() => {
       const getRepositorio = async () => {
         try {
@@ -19,14 +23,30 @@ const Portfolio = () => {
       getRepositorio()
   
   }, [])
-
+  
 
   return (
     <>
       <Header 
         title="Meus projetinhos"
-        image={portfolio}
       />
+
+      <main>
+        <div className={styles.mainPort}>
+        <img src={portfolio} alt="github" />
+        </div>
+        <div className={styles.trabalharRepos}>
+          <div>
+          {listaRepoQuero.map((repo) => (
+          <div className={styles.cardRepo} key={repo.id}>
+            <h3 className={styles.cardText}>{repo.name}</h3>
+            <a className={styles.cardLink} href={repo.html_url} target="_blank"></a> 
+          </div>
+        ))
+        }
+          </div>
+        </div>
+      </main>
 
       <div className={styles.repoConteiner}>
         {repos.map((repo) => (
